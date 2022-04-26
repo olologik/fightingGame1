13d8569e76a11523e26d8e2364b6060ec14a0b0c
olologik <102178778+olologik@users.noreply.github.com>
2022-04-26 21:36:05 +0300

    movement done

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-// рисуем залитый прямоугольник - границу блока canvas
-c.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 0.5
 
-const gravity = 0.2
-
-// создаём класс со методами 1позиционирование 2рисование 3обновление
+// создаём класс со методами 1конструктор 2рисование 3обновление
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.lastKey
     }
 
     draw() {
@@ -24,6 +22,7 @@ class Sprite {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -56,15 +55,112 @@ const enemy = new Sprite({
     }
 })
 
-console.log(player)
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    }
+}
 
 // создаем фунцию анимация, которая циклична и вызывает саму себя
 function animate() {
     window.requestAnimationFrame(animate)
+    // делаем фон блока canvas черным
     c.fillStyle = 'black'
+    // рисуем залитый прямоугольник - границу блока canvas
     c.fillRect(0,0, canvas.width, canvas.height)
+    // инициализируем создание блоков игрок и враг
     player.update()
     enemy.update()
-}
 
+    // задаем базовое значение скорости движения по горизонтали = 0
+    player.velocity.x = 0
+    enemy.velocity.x = 0
+
+    // Задаем условие движения по горизонтали при нажатии клавиш
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5
+    }
+
+    // то же самое для врага
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5
+    }
+}
+// запускаем анимацию
 animate()
+
+// Задаем фунцию "что происходит при нажатии клавиши"
+window.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -20
+            break
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -20
+            break
+    }
+    console.log(event.key)
+})
+
+// Задаем фунцию "что происходит при отпускании нажатой клавиши"
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 'w':
+            keys.w.pressed = false
+            break
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break
+    }
+    console.log(event.key)
+})
